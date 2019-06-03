9279973c7625c4d50c331cf95705de192a9a69f9
Aris Brink <arisbrink@Ariss-iMac.lan>
2019-06-03 11:45:44 +0300

    sync

--- src/app/sections/match-pages/match-page-leaders/match-page-leaders.component.ts
+++ src/app/sections/match-pages/match-page-leaders/match-page-leaders.component.ts
@@ -7,9 +7,120 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MatchPageLeadersComponent implements OnInit {
 
+  cellArray = [
+    { user: false },
+    { user: false },
+    { user: false },
+    { user: false },
+    { user: false },
+    { user: false },
+    { user: false },
+    { user: false },
+    { user: false },
+    { user: false },
+    { user: false },
+    { user: false },
+    { user: false },
+    { user: false },
+    { user: false },
+    { user: false },
+    { user: false },
+    { user: false },
+    { user: false },
+    { user: false },
+    { user: false },
+    { user: false },
+    { user: false },
+    { user: false },
+    { user: false },
+    { user: false },
+    { user: false },
+    { user: false },
+    { user: false },
+    { user: false },
+    { user: false },
+    { user: false },
+    { user: false },
+    { user: false },
+    { user: false },
+    { user: false },
+    { user: false },
+    { user: false },
+    { user: false },
+    { user: false },
+    { user: false },
+    { user: false },
+    { user: false },
+    { user: false },
+    { user: false },
+    { user: false },
+    { user: true },
+    { user: false },
+    { user: false },
+    { user: false },
+    { user: false },
+    { user: false },
+    { user: false },
+    { user: false },
+    { user: false },
+    { user: false },
+    { user: false },
+    { user: false },
+    { user: false },
+    { user: false },
+    { user: false },
+    { user: false },
+    { user: false },
+    { user: false },
+    { user: false },
+    { user: false },
+    { user: false },
+    { user: false },
+    { user: false },
+    { user: false },
+    { user: false },
+    { user: false },
+    { user: false },
+    { user: false },
+    { user: false },
+    { user: false },
+    { user: false },
+    { user: false },
+    { user: false },
+    { user: false },
+    { user: false },
+  ];
   constructor() { }
-
+  userRank: Number;
   ngOnInit() {
+    this.userRank = this.cellArray.findIndex(x=>x.user) + 1;
+  }
+
+  ngAfterViewInit(){
+    this.checkScroll();
+  }
+
+  checkScroll() {
+    let a = $(".leaders-scrollable-group");
+    let b = $('.leaders-user');
+
+    if (b.position().top < a.position().top) {
+      $(".leaders-table-user").addClass('leaders-table-user-up');
+    } else {
+      $(".leaders-table-user").removeClass('leaders-table-user-up');
+    }
+    // console.log("height:"+ (a.height())+" | "+ a.scrollTop()+" | "+b.position().top)
+    // let el = $('.leaders-user');
+    // if(el)
+    // console.log();
+    let dist = b.position().top - a.position().top - a.height() + 42;
+    if (dist >0) {
+      $(".leaders-table-user").addClass('leaders-table-user-down');
+    } else {
+      $(".leaders-table-user").removeClass('leaders-table-user-down');
+    }
+    // console.log(dist)
+    // let offset = $('.leaders-user').position().top;
   }
 
 }
