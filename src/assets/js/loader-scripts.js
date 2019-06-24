40f90cb627d4c54658f0e5e433fe6ed39eab5464
Aris Brink <arisbrink@Ariss-iMac.lan>
2019-06-24 14:04:01 +0300

    fix for loader

--- src/assets/js/loader-scripts.js
+++ src/assets/js/loader-scripts.js
@@ -1,38 +1,84 @@
 "use strict";
-$(document).ready(function(){
-	/*----------------------------------------
+// $(document).ready(function () {
+// 	/*----------------------------------------
+//      Pre Loader
+//      ----------------------------------------*/
+//     var anim;
+//     var elem = document.getElementById('lottie');
+//     var animData = {
+//         container: elem,
+//         renderer: 'svg',
+//         loop: false,
+//         autoplay: false,
+//         rendererSettings: {
+//             progressiveLoad: false,
+//             preserveAspectRatio: 'xMidYMid meet',
+//             imagePreserveAspectRatio: 'xMidYMid meet',
+//             title: 'TEST TITLE',
+//             description: 'TEST DESCRIPTION',
+//         },
+//         path: './assets/json/loader.json'
+//     };
+//     // lottie.setQuality('low');
+//     anim = lottie.loadAnimation(animData);
+
+//     $(window).on('load', function () {
+    
+//      $('#lottie').fadeIn();
+//      anim.play();
+//      //    var lines = document.getElementById('lines');
+ 
+//      console.log("-- #lines fadeOut");
+//      $('#lines').fadeOut();
+ 
+//      setTimeout(function () {
+//          $('.loader-wrapper').fadeOut('slow');
+//          $('.loader-wrapper').remove('slow');
+//      }, 5000)
+ 
+ 
+//      });
+// });
+
+    $(window).on('load', function () {
+       /*----------------------------------------
      Pre Loader
      ----------------------------------------*/
-     var anim;
-     var elem = document.getElementById('lottie');      
-     var animData = {
-         container: elem,
-         renderer: 'svg',
-         loop: false,
-         autoplay: false,
-         rendererSettings: {
-             progressiveLoad:false,
-             preserveAspectRatio: 'xMidYMid meet',
-             imagePreserveAspectRatio: 'xMidYMid meet',
-             title: 'TEST TITLE',
-             description: 'TEST DESCRIPTION',
-         },
-         path: './assets/json/loader.json'
-     };
-     // lottie.setQuality('low');
-     anim = lottie.loadAnimation(animData);
+    var anim;
+    var elem = document.getElementById('lottie');
+    var animData = {
+        container: elem,
+        renderer: 'svg',
+        loop: false,
+        autoplay: false,
+        rendererSettings: {
+            progressiveLoad: false,
+            preserveAspectRatio: 'xMidYMid meet',
+            imagePreserveAspectRatio: 'xMidYMid meet',
+            title: 'TEST TITLE',
+            description: 'TEST DESCRIPTION',
+        },
+        path: './assets/json/loader.json'
+    };
+    // lottie.setQuality('low');
+    anim = lottie.loadAnimation(animData);
 
-    $(window).on('load', function() {
+    console.log("-- anim loaded");
+    console.log("-- App loaded");
+
+    console.log("-- #lottie fadeIn");
 
     $('#lottie').fadeIn();
-       anim.play();
+    anim.play();
     //    var lines = document.getElementById('lines');
-       $('#lines').fadeOut();
-       
-      setTimeout(function(){
-$       ('.loader-wrapper').fadeOut('slow');
+
+    console.log("-- #lines fadeOut");
+    $('#lines').fadeOut();
+
+    setTimeout(function () {
+        $('.loader-wrapper').fadeOut('slow');
         $('.loader-wrapper').remove('slow');
-      },5000)
-        
+    }, 5000)
+
+
     });
-});
