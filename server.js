0993e27e4b6d63527f05004130bd406acba2ca64
aribrink <aris@bedbugstudio.com>
2019-09-28 00:07:42 +0300

    lets try this

--- server.js
+++ server.js
@@ -1,24 +1,38 @@
-//Install express server
-var compression = require('compression');
-const express = require('express');
-const path = require('path');
+"use strict";
+const express = require("express");
+const compression = require("compression");
+
+const _port = process.env.PORT || 8080;
+const _app_folder = 'dist/sportimo';
 
 const app = express();
-
 app.use(compression());
 
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/sportimo'));
+const _environment = process.env.ENVIRONMENT;
 
-app.get('/*', function(req,res) {
-    if (req.secure) {
-        res.sendFile(path.join(__dirname+'/dist/sportimo/index.html'));
-    }else{
-        res.redirect('https://' + req.headers.host + req.url);
-    }
+// ---- SERVE STATIC FILES ---- //
+app.get('*.*', express.static(_app_folder, {maxAge: '1y'}));
+
+// ---- SERVE APLICATION PATHS ---- //
+app.all('*', function (req, res) {
+    res.status(200).sendFile(`/`, {root: _app_folder});
 });
 
+// if (_environment === 'production') {
+    app.enable('trust proxy');
+    app.use(function (req, res, next) {
+        console.log(req.secure);
+        if (req.secure) {
+            // https request, nothing to handle
+            next();
+        } else {
+            // this is an http request, redirect to https
+            res.redirect(301, 'https://' + req.headers.host + req.url);   
+        }
+    });
+// }
 
-
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+// ---- START UP THE NODE SERVER  ----
+app.listen(_port, function () {
+    console.log("Node Express server for " + app.name + " listening on http://localhost:" + _port);
+});
