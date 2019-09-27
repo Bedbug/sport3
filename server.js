834a07cc2156ffb6b7d42e9c29a2a5ca03ac408d
Aris Brink <aris@bedbugstudio.com>
2019-09-28 00:53:09 +0300

    more logs removal

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const _environment = process.env.ENVIRONMENT;
 if (_environment === 'production') {    
     app.enable('trust proxy');
     app.use(function (req, res, next) {
-        console.log(req.secure);
+
         if (req.secure) {
             // https request, nothing to handle
             next();
