0576f09461ae0f86e8f55c227d9cf75140977d9f
RabidRabbit <aris@bedbugstudio.com>
2020-07-21 14:11:57 +0300

    calc on resize

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -19,6 +19,17 @@ $(document).ready(function(){
     // });
 });
 
+$( window ).resize(function() {
+  console.log( "<div>Handler for .resize() called.</div>" );
+  this.calculateHeight();
+});
+
+function calculateHeight(){
+  vh = window.innerHeight * 0.01;
+  // Then we set the value in the --vh custom property to the root of the document
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+}
+
 // let defferedPrompt;
 window.addEventListener('beforeinstallprompt', (e) => {
   // Stash the event so it can be triggered later.
