162927ab8c05c39317e6ab0235f0109ae399acdc
RabidRabbit <aris@bedbugstudio.com>
2020-08-27 17:32:32 +0300

    more fixes

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -19,10 +19,10 @@ $(document).ready(function(){
     // });
 });
 
-$('input').on('focus', function(e) {
-  e.preventDefault(); e.stopPropagation();
-  window.scrollTo(0,0); //the second 0 marks the Y scroll pos. Setting this to i.e. 100 will push the screen up by 100px. 
-});
+// $('input').on('focus', function(e) {
+//   e.preventDefault(); e.stopPropagation();
+//   window.scrollTo(0,0); //the second 0 marks the Y scroll pos. Setting this to i.e. 100 will push the screen up by 100px. 
+// });
 
 $( window ).resize(function() {
   console.log( "<div>Handler for .resize() called.</div>" );
