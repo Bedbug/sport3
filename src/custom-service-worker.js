0b716a6943b6dac0f920996872c3265dd5ca57be
themisbrink <themisbrnk@gmail.com>
2021-07-19 16:02:25 +0300

    Update custom-service-worker.js

--- src/custom-service-worker.js
+++ src/custom-service-worker.js
@@ -28,26 +28,10 @@ importScripts('./ngsw-worker.js');
         }
     });
 
-    // self.addEventListener('notificationclick', (event) => {
-    //     console.log("This is custom service worker notificationclick method.");
-    //     console.log('Notification details: ', event.notification);
-    //     // Write the code to open
-    //     if (clients.openWindow && event.notification.data.url) {
-    //         event.waitUntil(clients.openWindow(event.notification.data.url));
-
-    //         // clickAckUrl
-    //         if (event.notification.data.clickAckUrl) {
-    //             // const body = { title: 'Angular PUT Request Example' };
-    //             // that.http.put < any > (event.notification.data.clickAckUrl, body).subscribe();
-    //             const Http = new XMLHttpRequest();
-    //             Http.open("PUT", event.notification.data.clickAckUrl);
-    //             Http.send();
-    //         }
-    //     }
-    // });
+    
     self.addEventListener('push',  (event) => {
         console.log('[Service Worker] Push Received.');
-        console.log(event.notification);
+        console.log(event);
         // const title = 'Push Codelab';
         // const options = {
         //   body: 'Yay it works.',
