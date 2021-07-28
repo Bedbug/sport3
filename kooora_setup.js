c2db15dd881bafa8c338cd35f04b7d677473541d
RabidRabbit <aris@bedbugstudio.com>
2021-07-28 17:55:41 +0300

    sync

--- kooora_setup.js
+++ kooora_setup.js
@@ -2,11 +2,22 @@
 
 const replace = require('replace-in-file');
 
-// Index -  Chagne Favicon
+// Index -  Change Index.html
 const options = {
   files: 'src/index.html',  
-  from: ['./assets/icons/icon-512x512.png', './assets/icons/icon-192x192.png', './assets/icons/icon-72x72.png', 'Sportimo 3 - The way to watch football', '<!-- <meta name="facebook-domain-verification" content="b6q9nsqsnafg0ypfuh9nca32600gzk" /> -->'],  
-  to: ['./assets/icons/kooora_logo.png', './assets/icons/kooora_logo.png', './assets/icons/kooora_logo.png', 'Kooora Football Legends', '<meta name="facebook-domain-verification" content="b6q9nsqsnafg0ypfuh9nca32600gzk" />'],
+  from: [
+    './assets/icons/icon-512x512.png', 
+    './assets/icons/icon-192x192.png', 
+    './assets/icons/icon-72x72.png', 
+    'Sportimo 3 - The way to watch football', 
+    '<!-- <meta name="facebook-domain-verification" content="b6q9nsqsnafg0ypfuh9nca32600gzk" /> -->'
+  ],   
+  to: [
+    './assets/icons/kooora_logo.png', 
+    './assets/icons/kooora_logo.png', 
+    './assets/icons/kooora_logo.png', 
+    'Kooora Football Legends', 
+    '<meta name="facebook-domain-verification" content="b6q9nsqsnafg0ypfuh9nca32600gzk" />'],
   countMatches: true,
 };
 
@@ -18,11 +29,11 @@ try {
     console.error('Error occurred:', error);
   }
 
-// Index -  Default language
+// Config -  Default Client
 const options1 = {
-  files: ['src/assets/config/development.json','src/assets/config/production.json'],
-  to: ['"CLIENT_ID": "5eb9a2a3b0a6860017aa5852"'],
+  files: ['src/assets/config/development.json','src/assets/config/production.json'],  
   from: ['"CLIENT_ID": "5be2bfc7135a3e1e2d4a637f"'],
+  to: ['"CLIENT_ID": "5eb9a2a3b0a6860017aa5852"'],
   countMatches: true,
 };
 
@@ -33,53 +44,3 @@ try {
   catch (error) {
     console.error('Error occurred:', error);
   }
-
-  // Index -  Variables
-// const options2 = {
-//     files: 'src/variables.scss',
-//     to: ['$theme-header-color: #ffc300;','$theme-header-logo: url("~src/assets/images/sportimo/logo-pabetoop.jpg");','$theme-header-text-color: #333333;'],
-//     from: ['$theme-header-color: RGBA(10, 28, 45, 1.00);', '$theme-header-logo: url("~src/assets/images/sportimo/icon-logo.svg");','$theme-header-text-color: white;'],
-//     countMatches: true,
-//   };
-  
-//   try {
-//       const results = replace.sync(options2);
-//       console.log('Replacement results:', results);
-//     }
-//     catch (error) {
-//       console.error('Error occurred:', error);
-//     }
-
-    // Change Texts
-// const options3 = {
-//     files: 'src/assets/i18n/en.json',
-//     to: [
-//       '"1": "Welcome to Pabetoop!"',
-//       'app-name": "pabetoop"',
-//     '"Welcome to Sportimo!": "Welcome to Pabetoop!"',
-//     '"Sportimo Winners": "Pabetoop Winners"'],
-//     from: [
-//       '"1": "Welcome to SPORTIMO!"',
-//       'app-name": "SPORTIMO"',
-//       '"Welcome to Sportimo!": "Welcome to Sportimo!"',
-//     '"Sportimo Winners": "Sportimo Winners"'],
-//     countMatches: true,
-//   };
-  
-//   try {
-//       const results = replace.sync(options3);
-//       console.log('Replacement results:', results);
-//     }
-//     catch (error) {
-//       console.error('Error occurred:', error);
-//     }
-
-//   1. root/src/variables.scss
-//   comment out Sportimo section
-//   comment in Pabetoop section
-  
-// 2. root/src/assets/i18n/en.json
-//   change key app-name value to "pabetoop"
-  
-// 3. root/src/assets/i18n/fa.json
-//   change key app-name value to "پا به توپ"
