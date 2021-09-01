58cf8a6afb4d575eff0d48fe58a11ac892208d55
themisbrink <themisbrnk@gmail.com>
2021-09-01 14:04:53 +0300

    Update tribuna_setup.js

--- tribuna_setup.js
+++ tribuna_setup.js
@@ -43,3 +43,41 @@ try {
   catch (error) {
     console.error('Error occurred:', error);
   }
+
+  // Manifest
+  const options2 = {
+    files: 'src/manifest.webmanifest',  
+    from: [
+      '"name": "Sportimo",', 
+      '"short_name": "Sportimo",', 
+      'assets/icons/icon-72x72.png', 
+      'assets/icons/icon-96x96.png',
+      'assets/icons/icon-128x128.png',
+      'assets/icons/icon-144x144.png',
+      'assets/icons/icon-152x152.png',
+      'assets/icons/icon-192x192.png',
+      'assets/icons/icon-384x384.png',
+      'assets/icons/icon-512x512.png'
+    ],   
+    to: [
+      '"name": "Tribuna",', 
+      '"short_name": "Tribuna",', 
+      'assets/icons/tribuna_logo.png', 
+      'assets/icons/tribuna_logo.png', 
+      'assets/icons/tribuna_logo.png', 
+      'assets/icons/tribuna_logo.png', 
+      'assets/icons/tribuna_logo.png', 
+      'assets/icons/tribuna_logo.png', 
+      'assets/icons/tribuna_logo.png', 
+      'assets/icons/tribuna_logo.png'
+    ],
+    countMatches: true,
+  };
+  
+  try {
+      const results = replace.sync(options2);
+      console.log('Replacement results:', results);
+    }
+    catch (error) {
+      console.error('Error occurred:', error);
+    }
