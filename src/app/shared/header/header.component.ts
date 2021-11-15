f990d2af7fe0d0ec487e0f57a43ed92f7c6f6cca
RabidRabbit <aris@bedbugstudio.com>
2021-11-15 12:01:03 +0200

    sync

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -52,15 +52,20 @@ export class HeaderComponent implements OnInit {
   deferredPrompt: any;
   showButton = false;
 
-  // @HostListener('window:beforeinstallprompt', ['$event'])
-  // onbeforeinstallprompt(e) {    
-  //   // console.log(e);
-  //   // Prevent Chrome 67 and earlier from automatically showing the prompt
-  //   e.preventDefault();
-  //   // Stash the event so it can be triggered later.
-  //   this.deferredPrompt = e;
-  //   this.showButton = true;
-  // }
+  @HostListener('window:beforeinstallprompt', ['$event'])
+  onbeforeinstallprompt(e) {    
+    console.log("prompt with delay");
+    
+    if(defPrompt)
+    {
+      this.deferredPrompt = defPrompt;
+      this.showButton = true;
+    }else{
+      this.deferredPrompt = e;
+      this.showButton = true;
+    }
+    
+  }
 
 
   addToHomeScreen() {
