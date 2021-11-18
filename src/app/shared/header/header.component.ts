b8ab8efe24e5e36957562967cbc645c7cb74928d
RabidRabbit <aris@bedbugstudio.com>
2021-11-18 19:04:40 +0200

    sync

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -67,7 +67,7 @@ export class HeaderComponent implements OnInit {
       clearTimeout(this.installPopupTimeout)
       this.installPopupTimeout = setTimeout(() => {
         this.showInstallPopup();
-      }, 60);
+      }, 6000);
     } else {
       this.deferredPrompt = e;
       this.showButton = true;
@@ -104,7 +104,7 @@ export class HeaderComponent implements OnInit {
       this.showButton = true;
       this.installPopupTimeout = setTimeout(() => {
         this.showInstallPopup();
-      }, 60);
+      }, 6000);
     }
     // this.showButton = true;
     // this.showInstallPopup();
